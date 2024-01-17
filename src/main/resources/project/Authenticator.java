e8c5eeb69b28b3a0b643d9b370380fab027157c2
abell370 <ituniverse.sandbox@gmail.com>
2024-01-17 17:20:25 +0100

    +test

--- src/main/resources/project/Authenticator.java
+++ src/main/resources/project/Authenticator.java
@@ -12,23 +12,16 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
 public class Authenticator {
-    public static boolean isAuthenticate(String login, String password) throws NoSuchAlgorithmException {
-        InputStream inputStream = Authenticator.class.getClassLoader().getResourceAsStream("users.json");
-        boolean find = false;
-        if (inputStream != null) {
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-            JSONTokener tokener = new JSONTokener(bufferedReader);
-            JSONArray users = new JSONArray(tokener);
-            for(int i = 0; i < users.length(); i++) {
-                JSONObject user = (JSONObject) users.get(i);
-                String storedSalt = (String) user.get("salt");
-                String passwordHashed = SecurityUtils.getSecurePassword(password, Base64.getDecoder().decode(storedSalt));
-                if (user.get("login").equals(login) && user.get("password").equals(passwordHashed)) {
-                    find = true;
-                    break;
-                }
-            }
-        }
-        return find;
-    }
+
+    private int privateField;
+    private int secondPrivateField;
+    public void fakeOne(){}
+    public void fakeTwo(){}
+    public void fakeThree(){}
+    public void fakeFour(){}
+
 }
+
+
+
+
